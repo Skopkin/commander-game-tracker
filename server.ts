5539de5ae417e480d6d717f69e7fb804f12bc5d0
Sam Kopkin <skopkin91@gmail.com>
2024-09-03 11:16:58 -0400

    Added placeholder home page and css to enable testing of Google login API

--- server.ts
+++ server.ts
@@ -1,9 +1,19 @@
 import express, { Application } from "express";
 import Server from "./src/index";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 const app: Application = express();
 const server: Server = new Server(app);
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use((express.static(__dirname + '/public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/home.html'));
+})
 
 app
     .listen(PORT, "localhost", function () {
